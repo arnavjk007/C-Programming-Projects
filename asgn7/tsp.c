9a0bd046d2d05c5330e62c0a1c07e9b144b4d554
Arnav Kumar <144872754+arnavjk007@users.noreply.github.com>
2024-06-12 22:28:11 -0700

    Update tsp.c

--- asgn7/tsp.c
+++ asgn7/tsp.c
@@ -13,60 +13,39 @@ Path *final_path = NULL, *temp = NULL;
 void dfs(uint32_t v, Graph *g) {
     graph_visit_vertex(g, v);
     path_add(temp, v, g);
-    //path_print(temp, stdout, g);
-    //printf("adding path %d\n", v);
-    //    printf("V Value : %u ", v);
     for (uint32_t i = 0; i < total_v; i++) {
-        //	printf("Procesing v value %u, i value : %u\n", v, i);
         if (graph_get_weight(g, v, i)) {
             if (i == 0) {
                 if (path_vertices(temp) < total_v) {
-                    //printf("In Complete - nodes %i\n", path_vertices(temp));
                     continue;
                 }
                 // We are back at start
-                //graph_add_vertex(g, graph_get_vertex_name(g, 0), total_v);
                 path_add(temp, 0, g);
-                //printf("Complete\n");
-                //printf("total_weight : %u\n", total_weight);
-                //printf("Path distance: %u\n", path_distance(temp));
                 if (path_distance(temp) < total_weight) {
-                    //                    printf("CHANGED WIEGHT \n");
                     total_weight = path_distance(temp);
-                    //path_print(temp, stdout, g);
-                    //printf("total_weight is %u\n", total_weight);
                     path_copy(final_path, temp);
-                    //path_print(final_path, stdout, g);
                 }
                 path_remove(temp, g);
                 return;
             }
 
             if (graph_visited(g, i)) {
-                //		    printf("Already visited .. ignore for v value %u, i value %u\n", v, i);
                 continue;
             }
-            //            printf("calling dfs with value %d\n", i);
-
             dfs(i, g);
-            //	    printf("UNVISITING v : %u, i : %u\n", v, i);
             graph_unvisit_vertex(g, i);
             path_remove(temp, g);
         }
     }
     graph_unvisit_vertex(g, v);
-    //printf("\n");
-    //    printf("UNIVISTED : %u \n", v);
 }
 
 int main(int argc, char **argv) {
-    //printf("Entering program\n");
     bool read_from_file = false, write_to_file = false;
     bool directed = false;
     FILE *f;
     FILE *wf;
     int i = 1;
-    //printf("Reading arguments\n");
 
     while (i < argc) {
         if (strcmp(argv[i], "-o") == 0) {
@@ -98,14 +77,9 @@ int main(int argc, char **argv) {
         //increment
         i++;
     }
-
-    //printf("Opening file with fopen\n");
-    //f = fopen(argv[argc - 1], read_from_file ? "r" : "w");
     uint32_t vertices = 0, index = 0, paths = 0;
 
-    //printf("Reading number of vertices\n");
     //checks if num vertices is valid number
-
     if (fscanf(read_from_file ? f : stdin, "%u\n", &vertices) != 1) {
         fprintf(stderr, "tsp: error reading number of vertices\n");
         fclose(f);
@@ -117,13 +91,10 @@ int main(int argc, char **argv) {
     final_path = path_create(PATH_MAX);
     temp = path_create(PATH_MAX);
     total_v = vertices;
-    //printf("Number of vertices: %u\n", vertices);
-
-    //printf("Creating graph..\n");
+    
     // initializes graph with read in number of vertices and directed bool var
     Graph *g = graph_create(vertices, directed);
 
-    //printf("Reading names for graph..\n");
     // reads in names as words and adds them w/ corresponding vertex
     while ((fgets(line, 255, read_from_file ? f : stdin)) != NULL && index < vertices) {
         //printf("Word: %s\n", line);
@@ -144,19 +115,13 @@ int main(int argc, char **argv) {
     sscanf(word, "%u", &paths);
 
     while ((fgets(line, 255, read_from_file ? f : stdin)) != NULL && index < paths) {
-        //       printf("Path %u: %s\n", index, line);
-
         word = strtok(line, " ");
-
         sscanf(word, "%d", &start);
         word = strtok(NULL, " ");
-
         sscanf(word, "%d", &end);
         word = strtok(NULL, " ");
-
         sscanf(word, "%d", &weight);
         word = strtok(NULL, " ");
-
         graph_add_edge(g, start, end, weight);
         if (!directed && graph_get_weight(g, start, end) != 0) {
             graph_add_edge(g, end, start, weight);
@@ -165,16 +130,13 @@ int main(int argc, char **argv) {
     }
 
     //finding lowest weight path
-    //printf("Ready to Traverse the graph\n\n");
     dfs(0, g);
 
-    //printf("total_weight is %u ... path size is %i\n", total_weight, path_vertices(final_path));
     if (total_weight != INT_MAX) {
         path_print(final_path, write_to_file ? wf : stdout, g);
         fprintf(write_to_file ? wf : stdout, "Total Distance: %u\n", total_weight);
     } else
         fprintf(write_to_file ? wf : stdout, "No path found! Alissa is lost!\n");
-    //graph_print(g);
 
     graph_free(&g);
     path_free(&final_path);
